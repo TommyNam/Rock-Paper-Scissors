d0d495cb80d58a7f5132416b5638f520585ee6c7
Tommy-Nam <tommy-nam@R2Hs-MacBook-Pro.local>
2025-11-13 16:09:51 -0500

    Saving

---
+++ script.js
@@ -0,0 +1,8 @@
+let userInput = document.querySelector('#user-input');
+let submitBtn = document.querySelector('.submit');
+let results = document.querySelector('#results');
+
+
+submitBtn.addEventListener('click', () => {
+    let userInput = userInput.value;
+});
